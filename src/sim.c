95aa05b157bb7d5d71996792ff3a26970e41bcf4
Nishant Ghosh <nghosh@ece002.campus.ece.cmu.local>
2016-01-18 19:12:52 -0500

    Added R-type instructions

--- src/sim.c
+++ src/sim.c
@@ -96,11 +96,137 @@ void process_instruction()
 	break;
 /*************************************************************/
 /*** specify the remaining dcd_funct cases below this line ***/
-	
-	
-	
-	
-	
+      case SUBOP_AND:
+       if(dcd_rd!=0)
+         NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rs] & CURRENT_STATE.REGS[dcd_rt];
+       NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+      break;
+     case SUBOP_DIV:
+       if(dcd_rt != 0){
+        NEXT_STATE.HI = (int)CURRENT_STATE.REGS[dcd_rs] % (int)CURRENT_STATE.REGS[dcd_rt];
+        NEXT_STATE.LO = (int)CURRENT_STATE.REGS[dcd_rs] / (int)CURRENT_STATE.REGS[dcd_rt];
+      }
+      else
+        printf("Division by zero. Value unpredictable");
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+      break;
+     case SUBOP_DIVU:
+       if(dcd_rt != 0){
+        NEXT_STATE.HI = CURRENT_STATE.REGS[dcd_rs] % CURRENT_STATE.REGS[dcd_rt];
+        NEXT_STATE.LO = CURRENT_STATE.REGS[dcd_rs] / CURRENT_STATE.REGS[dcd_rt];
+      }
+      else
+        printf("Division by zero. Value unpredictable");
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+      break;
+     case SUBOP_JALR:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.PC + 8;
+      if(!(CURRENT_STATE.REGS[dcd_rs] & 0x03))
+        NEXT_STATE.PC = CURRENT_STATE.REGS[dcd_rs];
+      else
+        printf(" Address not word aligned ");
+     break;
+     case SUBOP_JR:
+      if(!(CURRENT_STATE.REGS[dcd_rs] & 0x03))
+        NEXT_STATE.PC = CURRENT_STATE.REGS[dcd_rs];
+      else{
+        printf(" Address not word aligned ");
+        NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+      }
+     break;
+     case SUBOP_MFHI:
+      if(dcd_rd != 0)
+       NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.HI;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_MFLO:
+      if(dcd_rd != 0)
+       NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.LO;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_MTHI:
+      if(dcd_rs != 0)
+        NEXT_STATE.HI = CURRENT_STATE.REGS[dcd_rs];
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_MTLO:
+      if(dcd_rs != 0)
+        NEXT_STATE.LO = CURRENT_STATE.REGS[dcd_rs];
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_MULT:
+      prod = ((int)CURRENT_STATE.REGS[dcd_rs] * (int)CURRENT_STATE.REGS[dcd_rt]);
+      NEXT_STATE.HI = (prod >> 31) & 0xFFFFFFFF;
+      NEXT_STATE.LO = (prod) & 0xFFFFFFFF;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_MULTU:
+      NEXT_STATE.HI = ((CURRENT_STATE.REGS[dcd_rs] * CURRENT_STATE.REGS[dcd_rt]) >> 31);
+      NEXT_STATE.LO = (CURRENT_STATE.REGS[dcd_rs] * CURRENT_STATE.REGS[dcd_rt]) & 0xFFFFFFFF;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_NOR:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = ~(CURRENT_STATE.REGS[dcd_rs] | CURRENT_STATE.REGS[dcd_rt]);
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_OR:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = (CURRENT_STATE.REGS[dcd_rs] | CURRENT_STATE.REGS[dcd_rt]);
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SLL:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rt] << dcd_shamt;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SLLV:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rt] << (CURRENT_STATE.REGS[dcd_rs] & 0x1F);
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SLT:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = ((int)CURRENT_STATE.REGS[dcd_rs] < (int)CURRENT_STATE.REGS[dcd_rt]) ? 1 : 0;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SLTU:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = (CURRENT_STATE.REGS[dcd_rs] < CURRENT_STATE.REGS[dcd_rt]) ? 1 : 0;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SRA:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = (int)CURRENT_STATE.REGS[dcd_rt] >> dcd_shamt;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SRAV:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = (int)CURRENT_STATE.REGS[dcd_rt] >> (CURRENT_STATE.REGS[dcd_rs] & 0x1F);
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SRL:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rt] >> dcd_shamt;
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SRLV:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rt] >> (CURRENT_STATE.REGS[dcd_rs] & 0x1F);
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_SUB:
+     case SUBOP_SUBU:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rs] - CURRENT_STATE.REGS[dcd_rt];
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
+     case SUBOP_XOR:
+      if(dcd_rd != 0)
+        NEXT_STATE.REGS[dcd_rd] = CURRENT_STATE.REGS[dcd_rt] ^ CURRENT_STATE.REGS[dcd_rs];
+      NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+     break;
 /*** specify the remaining dcd_funct cases above this line ***/
 /*************************************************************/
       default:
